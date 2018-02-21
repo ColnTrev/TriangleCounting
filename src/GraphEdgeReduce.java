da61c651bf03a9da667e34205ed1dc8d7104f87d
Collin Gordon <colntrev@gmail.com>
2018-02-21 11:12:41 -0800

    updating files

--- src/GraphEdgeReduce.java
+++ src/GraphEdgeReduce.java
@@ -2,18 +2,35 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by colntrev on 2/18/18.
  */
-public class GraphEdgeReduce extends Reducer<IntWritable,IntWritable,IntWritable,IntWritable> {
+public class GraphEdgeReduce extends Reducer<IntWritable,IntWritable,EdgePair,IntWritable> {
     @Override
     protected void reduce(IntWritable node, Iterable<IntWritable> edges, Context context) throws IOException,InterruptedException {
+
+        List<IntWritable> cache = new ArrayList<>(); // add values from iterable here since iterable passes once
         //write edges in K,V pairs with K
         // example [2,1],(-)
+        for(IntWritable edge : edges){
+            cache.add(edge);
+            EdgePair e = new EdgePair(node.get(), edge.get());
+            context.write(e,null);
+        }
 
         //write edges with V,V pairs with their K connection
         // example [1,3], [2]
+        for(int i = 0; i < cache.size(); i++){
+            for(int j = 0; j < cache.size(); j++){
+                if(cache.get(i).get() != cache.get(j).get()){
+                    EdgePair e = new EdgePair(cache.get(i).get(),cache.get(j).get());
+                    context.write(e, node);
+                }
+            }
+        }
     }
 }
--- src/TriangleMap.java
+++ src/TriangleMap.java
@@ -1,5 +1,34 @@
-/**
- * Created by colntrev on 2/18/18.
- */
-public class TriangleMap {
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TriangleMap extends Mapper<EdgePair,IntWritable,EdgePair,IntWritable>{
+    private final Map<EdgePair,IntWritable> edgeMap = new HashMap<>();
+    @Override
+    protected void setup(Context context) throws IOException,InterruptedException{
+        super.setup(context);
+        Configuration conf = context.getConfiguration();
+        Path edges = new Path(conf.get("graphEdge.path"));
+        try(SequenceFile.Reader reader = new SequenceFile.Reader(conf,SequenceFile.Reader.file(edges))) {
+            EdgePair edge = new EdgePair();
+            IntWritable connector = new IntWritable();
+            while(reader.next(edge, connector)) {
+                edgeMap.put(edge, connector);
+            }
+        }
+    }
+
+    @Override
+    public void map(IntWritable key, IntWritable value, Context context) throws IOException,InterruptedException {
+        for(HashMap.Entry<EdgePair,IntWritable> entry : edgeMap.entrySet()){
+            context.write(entry.getKey(), entry.getValue());
+        }
+
+    }
 }
